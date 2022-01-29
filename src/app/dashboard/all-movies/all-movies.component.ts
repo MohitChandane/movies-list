b14aba977879be5aa437672204cf2bd40514ef8d
Mohit <mohit.chandane@coreviewsystems.com>
2022-01-29 19:40:14 +0530

    removed comments

--- src/app/dashboard/all-movies/all-movies.component.ts
+++ src/app/dashboard/all-movies/all-movies.component.ts
@@ -15,19 +15,13 @@ export class AllMoviesComponent implements OnInit {
   atBottom: boolean = false;
   page: any = 1;
   constructor(private apiService: ApiService,
-    @Inject(DOCUMENT) private document: Document,
-    // @Inject(WINDOW) private window: Window
+    @Inject(DOCUMENT) private document: Document
     ) { }
 
-  // @HostListener("window:scroll", [$event])
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(event: any) {
     const offset = this.document.documentElement.scrollTop || this.document.body.scrollTop || 0;
     console.log(offset);
-    // console.log(event);
-    // console.log(window.innerHeight)
-
-    // console.log('this.page',this.page);
   }
 
   ngOnInit(): void {
@@ -54,31 +48,13 @@ export class AllMoviesComponent implements OnInit {
     }, error => {})
   }
   onScroll(data : any){
-    // console.log('data', data)
-
-    // if ((data.target.scrollHeight - 5) <= data.target.scrollTop + data.target.offsetHeight) {
-    //   console.log("")
-    // }
-
     if ((data.target.scrollHeight - 5) <= data.target.scrollTop + data.target.offsetHeight) {
       this.atBottom = true;
-      console.log("at bottom")
       this.page = this.page +1
-      console.log("this.page", this.page)
       // this.callFuntion();
     } else {
-      console.log("at top")
-      // this.page = this.page -1
-      // console.log("this.page", this.page)
       this.atBottom = false;
     }
   }
 
-  onScrollDown(event: any){
-    console.log('event down', event);
-  }
 
-  onScrollUp(event:any){
-    console.log('event up', event);
-  }
-}
